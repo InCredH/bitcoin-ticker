21bfc5dd898a770cba1c436a18d60ce105c8afe1
InCredH <85639817+InCredH@users.noreply.github.com>
2022-07-19 22:23:25 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'price_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+    );
+  }
+}
